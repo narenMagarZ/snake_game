dda19d2bf3562b8229491babfa51c19d477db356
narenMagarZ <narenmagarz98@gmail.com>
2023-04-21 21:10:21 +0545

    rename playground component to home component

--- src/app.tsx
+++ src/app.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
+import Home from './component/home';
 
 function App() {
   return (
-    <div className="App">
-
+    <div className="app">
+      <Home/>
     </div>
   );
 }
