c0d5583860a508645eebe18da6abf8fbdbacd038
narenMagarZ <narenmagarz98@gmail.com>
2023-04-23 20:38:31 +0545

    Implemented PauseGameBtn component which is a simple button with a 'Pause' label. The component does not take any props and is a static component.

---
+++ src/component/home/pause-game-btn.tsx
@@ -0,0 +1,13 @@
+
+
+function PauseGameBtn(){
+     return(
+          <button
+          className="border-0 rounded px-2 py-1"
+          >
+               Pause
+          </button> 
+     )
+}
+
+export default PauseGameBtn
