13cc32919b3cd53fc565337b4466b9db0f263919
narenMagarZ <narenmagarz98@gmail.com>
2023-04-24 21:48:59 +0545

    added interfaces and types

--- src/type.d.ts
+++ src/type.d.ts
@@ -1,9 +1,43 @@
 
 
 interface IStartGameBtn{
-     isGameStarted:boolean
-     setIsGameStarted:React.Dispatch<React.SetStateAction<boolean>>
+     gameStatus:TGameStatus
+     setGameStatus:React.Dispatch<React.SetStateAction<TGameStatus>>
+}
+interface IEndGameBtn{
+     gameStatus:TGameStatus
+     setGameStatus:React.Dispatch<React.SetStateAction<TGameStatus>>
 }
 interface ISnake{
      snake:React.Ref<HTMLDivElement>
+     snakeBlock:any[]
+}
+interface IBlock{
+     x:number
+     y:number
+}
+type TSnakeDir = 'backward'|'forward'|'upward'|'downward'
+
+type TAxis = 'x'|'y'
+type TDirection = 'pos'|'neg'
+
+interface ISnakeFood{
+     foodCoordinate:[number,number]
+}
+interface ISnakeBlock{
+     x:number
+     y:number
+}
+
+type TSnake = ISnakeBlock[]
+type TCoordinate = [number,number]
+
+interface IPlayground{
+     gameStatus:TGameStatus
+}
+type TGameStatus = 'idle'|'playing'|'paused'|'end'
+
+interface IPauseGameBtn {
+     gameStatus:TGameStatus
+     setGameStatus:React.Dispatch<React.SetStateAction<TGameStatus>>
 }
